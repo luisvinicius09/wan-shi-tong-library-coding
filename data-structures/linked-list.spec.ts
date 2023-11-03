da228f8414335695ee7c654d77d403cb710e3217
Luis V <luisvinicius0906@gmail.com>
2023-11-03 10:34:39 -0300

    Add: vitest as dependency and test files skeletons

---
+++ data-structures/linked-list.spec.ts
@@ -0,0 +1,5 @@
+import { describe, it } from 'vitest';
+
+describe('Linked List', () => {
+	it.todo('should add an element to the list', () => {});
+});
---
+++ data-structures/stack.spec.ts
@@ -0,0 +1,5 @@
+import { describe, it } from 'vitest';
+
+describe('Stack', () => {
+	it.todo('should add an element to the stack', () => {});
+});
