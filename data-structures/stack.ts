68baa326a5fb70dc1e80defcf1fc23318d8ec51f
Luis V <luisvinicius0906@gmail.com>
2023-11-03 09:05:01 -0300

    Add: basic file structure to stack

---
+++ data-structures/stack.ts
@@ -0,0 +1,19 @@
+import { LinkedList } from './linked-list';
+
+export class Stack<T> {
+	private list: LinkedList<T>;
+
+	size() {}
+
+	isEmpty() {}
+
+	clear() {}
+
+	push() {}
+
+	pop() {}
+
+	peek() {}
+
+	contains() {}
+}
